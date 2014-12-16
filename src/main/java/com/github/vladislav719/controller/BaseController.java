84dd78cdf7970de604c39eaf2b02f1787ea841fd
Baimurzin Vladislav <baimurzin.719@gmail.com>
2014-12-16 22:18:08 +0400

    First Commit

---
+++ src/main/java/com/github/vladislav719/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.github.vladislav719.controller;
+
+/**
+ * Created by Владислав on 16.12.2014.
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/github/vladislav719/model/Users.java
@@ -0,0 +1,7 @@
+package com.github.vladislav719.model;
+
+/**
+ * Created by Владислав on 16.12.2014.
+ */
+public class Users {
+}
